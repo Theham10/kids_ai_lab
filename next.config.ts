a0d05cad7be4b897db85d9c967a37e8f45a1c264
Your Name <your-email@example.com>
2026-02-01 21:40:37 +0900

    feat: Vercel Unification & System Cleanup 🧹🛠️✨

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.IS_STATIC === 'true' ? 'export' : undefined,
   images: {
     unoptimized: true,
   },
